71070164d522e5efc657f77d834234b361f553a3
Mxt@DEV <maxta.gamer@gmail.com>
2024-06-24 13:12:12 -0300

    update

--- API-register-of-users/src/app.ts
+++ API-register-of-users/src/app.ts
@@ -1,10 +1,12 @@
 import 'express-async-errors';
 import express, { json } from 'express';
+import helmet from 'helmet';
 import { usersRouter } from './routes/usersRouter.routes';
 import { HandleErrors } from './errors/HandleErrors.middleware';
 
 export const app = express();
 
+app.use(helmet());
 app.use(json());
 app.use('/users', usersRouter);
 app.use(HandleErrors.execute);
